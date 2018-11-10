71c456a15de4af632572268de63e4d636c084f8f
OctoMiao <lei.ma@homelike.cc>
2018-11-10 12:50:51 +0100

    init vue app

---
+++ app/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="full-width center-content">
+    <h1>Explain Structural Plasticity Like I am Five</h1>
+  </div>
+</template>
+
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
